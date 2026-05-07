3474a5bcd0a5737faf6bc2a142baaab9b4033bf9
038bkn <038bkn@gmail.com>
2026-05-07 16:39:07 +0900

    feat: ユーザーモデルに新しいカラムを追加し、UserSeederを作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'points',
+        'mode',
+        'icon_path',
     ];
 
     /**
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'テストユーザー',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password123'),
+            'points' => 0,
+            'mode' => 0,
+        ]);
+    }
+}
