58bd2d044ad59f846a4088830f4a9c3f3e1ad190
unknown <234059@lab-ocs.local>
2026-05-15 10:45:51 +0900

    feat:難易度に応じてポイント付与機能追加

--- resources/views/calendar_screen.blade.php
+++ resources/views/calendar_screen.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+    <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>カレンダー</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}"> <title>カレンダー</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
 
