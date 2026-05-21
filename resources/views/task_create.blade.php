041e3954af361256fb6f447f9c3e537150b2bbe7
unknown <234059@lab-ocs.local>
2026-05-21 13:37:47 +0900

    Fix:タスク登録画面　ナビゲーションバーの修正

--- resources/views/task_create.blade.php
+++ resources/views/task_create.blade.php
@@ -94,14 +94,22 @@
         </form>
 
         {{-- ボトムナビ --}}
-        <nav class="w-full flex justify-center items-center text-slate-600 flex-shrink-0 border-t border-[#9ecfde]" style="background-color: #b8e0e9; height: 80px;">
-            <div class="w-full max-w-2xl px-6 flex justify-between items-center">
-                <a href="/chat" class="flex flex-col items-center gap-1"><span>💬</span><span class="text-[10px] font-bold">ひとりごと</span></a>
-                <a href="/home" class="flex flex-col items-center gap-1"><span>🏠</span><span class="text-[10px] font-bold">ホーム</span></a>
-                <a href="/calendar" class="flex flex-col items-center gap-1 text-slate-900"><span>🗓️</span><span class="text-[10px] font-bold border-b-2 border-slate-900">ToDo</span></a>
-                <a href="/setting" class="flex flex-col items-center gap-1"><span>⚙️</span><span class="text-[10px] font-bold">設定</span></a>
-            </div>
-        </nav>
+        <nav class="fixed bottom-0 left-0 w-full h-[68px] bg-[#cdeef9] border-t border-[#b5d9e4] z-20">
+        <div class="w-full max-w-[390px] mx-auto h-full flex justify-around items-center">
+            <a href="/chat" class="flex flex-col items-center justify-center">
+                <img src="{{ asset('images/icon/chat.png') }}" class="w-16 h-16 object-contain">
+            </a>
+            <a href="/home" class="flex flex-col items-center justify-center">
+                <img src="{{ asset('images/icon/home.png') }}" class="w-16 h-16 object-contain">
+            </a>
+            <a href="/calendar" class="flex flex-col items-center justify-center">
+                <img src="{{ asset('images/icon/todo.png') }}" class="w-16 h-16 object-contain">
+            </a>
+            <a href="/setting" class="flex flex-col items-center justify-center">
+                <img src="{{ asset('images/icon/setting.png') }}" class="w-16 h-16 object-contain">
+            </a>
+        </div>
+    </nav>
     </div>
 
     <script src="{{ asset('js/task_create.js') }}"></script>
